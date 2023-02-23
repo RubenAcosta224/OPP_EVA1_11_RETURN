d84c57b368a293ae1af8e8fa245b78830a5321c0
RubenAcosta224 <124905283+RubenAcosta224@users.noreply.github.com>
2023-02-22 22:27:26 -0600

    Add files via upload

---
+++ EVA1_11_RETURN/src/eva1_11_return/EVA1_11_RETURN.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_11_return;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_11_RETURN {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int resu=square(4);
+        
+        System.out.println("Resultado: "+resu);
+        
+        
+    }
+    
+    public static int square(int num){
+            return num*num;
+        }
+    
+}
